e8141be4928a168f9ecd1a944012abd33501bfd0
Alexey Nikolaev <alexey_nikolaev@ymail.com>
2020-01-23 23:28:06 +0200

    antialias

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     //scene.add(pivot);
 
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer({ alpha: true });
+    var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 
     renderer.setSize( window.innerWidth, window.innerHeight );
     this.mount.appendChild( renderer.domElement );
